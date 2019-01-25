0558b1c1bdaa9d3606fc8e5869fcfb51573a8478
tomoki69386 <tomoki_69386@yahoo.co.jp>
2019-01-25 13:14:47 +0900

    Fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  get 'main/index'
-  get 'main/service'
-  get 'main/privacy'
+  get '/' => 'main#index'
+  get '/service' => 'main#service'
+  get '/privacy' => 'main#privacy'
 end
